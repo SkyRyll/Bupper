43e70908c18b7134f5a69fccd6e192349ebd2e1d
SkyRyll <skyryll1987@gmail.com>
2023-03-26 21:27:31 +0200

    working login query and redirect

--- website/server.js
+++ website/server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const md5 = require("md5");
 const path = require("path");
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const encoder = bodyParser.urlencoded();
 
+// expose static path
 app.use(express.static(path.join(__dirname, "static")));
 app.set("view engine", "ejs");
 
+// route pages
 app.get("/", (req, res) => {
     res.render("pages/index");
 });
@@ -24,6 +30,47 @@ app.get("/about", (req, res) => {
 app.get("/account", (req, res) => {
     res.render("pages/account");
 });
+app.get("/login", (req, res) => {
+    res.render("pages/login");
+});
+app.get("/register", (req, res) => {
+    res.render("pages/register");
+});
+
+// host app on port xxxx
 app.listen(port, () => {
     console.log(`App listening at port ${port}`);
+});
+
+//connect to local database
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "optik",
+});
+
+// connection test
+connection.connect(function (error) {
+    if (error) throw error;
+    else console.log("connection to database successful");
+});
+
+//login check and redirect
+app.post("/login", encoder, function (req, res) {
+    var username = req.body.username;
+    var password = md5(req.body.password);
+
+    connection.query(
+        "select * from accounts where username = ? and password = ?",
+        [username, password],
+        function (error, results, fields) {
+            if (results.length > 0) {
+                res.redirect("/account");
+            } else {
+                res.redirect("/");
+            }
+            res.end();
+        }
+    );
 });
